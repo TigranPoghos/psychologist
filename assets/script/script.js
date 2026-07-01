1be91cdc889cc91a86a148fad51f3f33c1b70da3
TigranPoghos <tigran2002poghosyan@gmail.com>
2026-07-01 23:09:53 +0300

    1

--- assets/script/script.js
+++ assets/script/script.js
@@ -37,4 +37,24 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 
+    gsap.registerPlugin(ScrollTrigger)
+
+    gsap.utils.toArray(".anim").forEach(elem => {
+        gsap.fromTo(elem, 
+            { opacity: 0, y: 20 }, 
+            { 
+            opacity: 1, 
+            y: 0, 
+            duration: 0.6, 
+            ease: "power2.out",
+                scrollTrigger: {
+                    trigger: elem,
+                    start: "top 90%",
+                    toggleActions: "play none none none"
+                }
+            }
+        );
+    });
+
+
 })
