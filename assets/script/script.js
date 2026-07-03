9fd60f21eae8314c2dd33a7c05ed786bb7a60dc1
TigranPoghos <tigran2002poghosyan@gmail.com>
2026-07-03 18:54:29 +0300

    1

--- assets/script/script.js
+++ assets/script/script.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", function(){
 
     
     const faqItems = document.querySelectorAll('.faq__item');
-
     faqItems.forEach(item => {
         item.addEventListener('click', () => {
             const isActive = item.classList.contains('active');
@@ -20,13 +19,12 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
     const slider = document.querySelector('.mySwiper');
-
     if (slider) {
         const slidesCount = slider.querySelectorAll('.swiper-slide').length;
-
         let maxSlides;
-
-        if (window.innerWidth <= 768) {
+        if (window.innerWidth <= 576) {
+            maxSlides = 2;
+        } else if (window.innerWidth <= 768) {
             maxSlides = 3;
         } else if (window.innerWidth <= 1024) {
             maxSlides = 4;
@@ -35,12 +33,7 @@ document.addEventListener("DOMContentLoaded", function(){
         } else {
             maxSlides = 6;
         }
-
-        const slidesPerView =
-            slidesCount >= maxSlides
-                ? maxSlides
-                : Math.max(3, slidesCount);
-
+        const slidesPerView = slidesCount >= maxSlides ? maxSlides : slidesCount;
         new Swiper('.mySwiper', {
             slidesPerView,
             spaceBetween: 20,
@@ -49,8 +42,41 @@ document.addEventListener("DOMContentLoaded", function(){
 
 
 
-    gsap.registerPlugin(ScrollTrigger)
 
+
+    let adminSwiper = null;
+
+    function initAdminSwiper() {
+        const slider = document.querySelector('.admin__slider');
+        if (!slider) return;
+
+        if (window.innerWidth <= 768) {
+            if (!adminSwiper) {
+                adminSwiper = new Swiper('.admin__slider', {
+                    slidesPerView: window.innerWidth <= 576 ? 'auto' : 2,
+                    spaceBetween: 20,
+                });
+            }
+        } else {
+            if (adminSwiper) {
+                adminSwiper.destroy(true, true);
+                adminSwiper = null;
+            }
+        }
+    }
+
+    initAdminSwiper();
+    window.addEventListener('resize', initAdminSwiper);
+
+
+
+
+
+
+
+
+
+    gsap.registerPlugin(ScrollTrigger)
     gsap.utils.toArray(".anim").forEach(elem => {
         gsap.fromTo(elem, 
             { opacity: 0, y: 20 }, 
@@ -67,6 +93,57 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         );
     });
+
+
+
+
+    $(document).ready(function () {
+        const burgerBtn = $('#nav-icon1');
+        const burger = $('.burger');
+        const overlay = $('.header__opacite');
+        const body = $('body');
+        const header = $('.header')
+
+        function openBurger() {
+            burgerBtn.addClass('open');
+            burger.addClass('active');
+            overlay.addClass('active');
+            body.addClass('hidden');
+            header.addClass('active')
+        }
+
+        function closeBurger() {
+            burgerBtn.removeClass('open');
+            burger.removeClass('active');
+            overlay.removeClass('active');
+            body.removeClass('hidden');
+            header.removeClass('active')
+        }
+
+        burgerBtn.on('click', function (e) {
+            e.stopPropagation();
+
+            if (burgerBtn.hasClass('open')) {
+                closeBurger();
+            } else {
+                openBurger();
+            }
+        });
+
+        burger.on('click', function (e) {
+            e.stopPropagation();
+        });
+
+        $(document).on('click', function () {
+            closeBurger();
+        });
+
+        $('.burger a').on('click', function () {
+            closeBurger();
+        });
+    });
+
+
 
 
 })
